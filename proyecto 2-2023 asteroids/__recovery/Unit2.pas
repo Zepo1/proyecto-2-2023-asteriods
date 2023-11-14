3c40616d8f8217b49e8112d28b901ebba798e78e
Zepo1 <136913834+Zepo1@users.noreply.github.com>
2023-11-13 21:08:12 -0400

    ayudaaaaaaa

---
+++ proyecto 2-2023 asteroids/__recovery/Unit2.pas
@@ -0,0 +1,24 @@
+unit Unit2;
+
+interface
+    type
+         nave = class
+          private
+             valor:Cardinal;
+          public
+            procedure direccion (d:string);
+         end;
+implementation
+
+{ nave }
+
+
+
+{ nave }
+
+procedure nave.direccion(d: string);
+begin
+
+end;
+
+end.
